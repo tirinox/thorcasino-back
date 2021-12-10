9e8bf63571468a501afe7b8d9aad3c5aceb21872
Tirinox <developer@tirinox.ru>
2021-12-10 20:16:31 +0300

    FastApi skeleton

--- src/main.py
+++ src/main.py
@@ -1,5 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+
 from lib.config import Config
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'hello': 'world'}
+
+
+@app.on_event('startup')
+async def on_start():
+    ...
+
+
+@app.on_event('shutdown')
+async def on_shutdown():
+    ...
+
 
 if __name__ == '__main__':
     c = Config()
-    print(c)
+    port = c.as_int('api.port', 8888)
+    host = c.as_str('api.host', '0.0.0.0')
+    reload = c.as_bool('api.reload', True)
+    uvicorn.run('main:app', port=port, host=host, reload=reload)
